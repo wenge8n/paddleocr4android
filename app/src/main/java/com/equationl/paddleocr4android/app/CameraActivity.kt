d1ee95b592f1ab402696b4496e2d863217a5267a
wenge8n <wenge8n@outlook.com>
2022-12-12 16:46:53 +0800

    init ocr

--- app/src/main/java/com/equationl/paddleocr4android/app/CameraActivity.kt
+++ app/src/main/java/com/equationl/paddleocr4android/app/CameraActivity.kt
@@ -17,7 +17,11 @@ import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.equationl.paddleocr4android.CpuPowerMode
+import com.equationl.paddleocr4android.OCR
+import com.equationl.paddleocr4android.OcrConfig
 import com.equationl.paddleocr4android.app.databinding.ActivityCameraBinding
+import com.equationl.paddleocr4android.callback.OcrInitCallback
 import java.nio.ByteBuffer
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
@@ -28,6 +32,7 @@ class CameraActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCameraBinding
 
+    private lateinit var ocr: OCR
     private lateinit var cameraExecutor: ExecutorService
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,6 +50,8 @@ class CameraActivity : AppCompatActivity() {
                 this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
         }
 
+        initOCR()
+
         cameraExecutor = Executors.newSingleThreadExecutor()
     }
 
@@ -53,6 +60,56 @@ class CameraActivity : AppCompatActivity() {
             android.R.id.home -> finish()
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun initOCR() {
+        ocr = OCR(this)
+
+        // 配置
+        val config = OcrConfig()
+        //config.labelPath = null
+
+        config.modelPath = "models/ch_PP-OCRv2" // 不使用 "/" 开头的路径表示安装包中 assets 目录下的文件，例如当前表示 assets/models/ocr_v2_for_cpu
+        //config.modelPath = "/sdcard/Android/data/com.equationl.paddleocr4android.app/files/models" // 使用 "/" 表示手机储存路径，测试时请将下载的三个模型放置于该目录下
+        config.clsModelFilename = "cls.nb" // cls 模型文件名
+        config.detModelFilename = "det_db.nb" // det 模型文件名
+        config.recModelFilename = "rec_crnn.nb" // rec 模型文件名
+
+        // 运行全部模型
+        // 请根据需要配置，三项全开识别率最高；如果只开识别几乎无法正确识别，至少需要搭配检测或分类其中之一
+        // 也可单独运行 检测模型 获取文本位置
+        config.isRunDet = true
+        config.isRunCls = true
+        config.isRunRec = true
+
+        // 使用所有核心运行
+        config.cpuPowerMode = CpuPowerMode.LITE_POWER_FULL
+
+        // 绘制文本位置
+        config.isDrwwTextPositionBox = true
+
+        // 1.同步初始化
+        /*ocr.initModelSync(config).fold(
+            {
+                if (it) {
+                    Log.i(TAG, "onCreate: init success")
+                }
+            },
+            {
+                it.printStackTrace()
+            }
+        )*/
+
+        // 2.异步初始化
+        ocr.initModel(config, object : OcrInitCallback {
+            override fun onSuccess() {
+                Log.i(TAG, "onSuccess: 初始化成功")
+            }
+
+            override fun onFail(e: Throwable) {
+                Log.e(TAG, "onFail: 初始化失败", e)
+            }
+        })
     }
 
     private fun startCamera() {
@@ -118,6 +175,7 @@ class CameraActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
+        ocr.releaseModel()
         cameraExecutor.shutdown()
     }
 
