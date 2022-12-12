61b383d19b686250c9b84a22037d817b7f0253a5
wenge8n <wenge8n@outlook.com>
2022-12-12 21:31:14 +0800

    add result activity

---
+++ app/src/main/java/com/equationl/paddleocr4android/app/ResultActivity.kt
@@ -0,0 +1,26 @@
+package com.equationl.paddleocr4android.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import com.equationl.paddleocr4android.app.databinding.ActivityResultBinding
+
+class ResultActivity : AppCompatActivity() {
+
+    private lateinit var viewBinding: ActivityResultBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewBinding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            android.R.id.home -> finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
